ea99edc62b5e1f73f6285b74d7e159c5ae2caf44
yanghai <yanghai0523@gmail.com>
2018-04-20 00:28:25 +0800

    查询商品

--- src/product/product.go
+++ src/product/product.go
@@ -4,18 +4,49 @@ import (
 	"net/http"
 	"fmt"
 	"utils"
+	"github.com/yanghai23/GoLib/aterr"
+	"initialize"
+	"encoding/json"
 )
 
 func GetOldProductInfo(w http.ResponseWriter, r *http.Request) {
 
 }
-func GetProductInfo(w http.ResponseWriter, r *http.Request) {
 
+type ServiceType struct {
+	ServiceType    string `josn:"serviceType"`
+	Ad             string `josn:"ad"`
+	Video          string `josn:"video"`
+	Speed          string `josn:"speed"`
+	Image          string `josn:"image"`
+	ServiceExplain string `josn:"serviceExplain"`
+	Products       []Product
 }
 
-func GenerateOrder(w http.ResponseWriter, r *http.Request)  {
+type Product struct {
+}
 
-	utils.OkStatus(w, 200,"支付成功", "{\"key\":\"\"}")
+func GetProductInfo(w http.ResponseWriter, r *http.Request) {
+	rows, err := initialize.Db.Query("SELECT serviceType,ad,video,speed,image,serviceExplain FROM ServiceTypeTab WHERE accountId = ?", accountId)
+	defer rows.Close()
+	var service []ServiceType
+	if err != nil {
+		panic("创建sql句柄错误")
+	}
+	for rows.Next() {
+		data := ServiceType{}
+		err = rows.Scan(&data.ServiceType, &data.Ad, &data.Video, &data.Speed, &data.Image, &data.ServiceExplain)
+		aterr.CheckErr(err)
+		service = append(service, data)
+	}
+	buf, err := json.Marshal(service)
+	aterr.CheckErr(err)
+	utils.OkStatus(w, 200, "服务类型", string(buf))
+}
+
+func GenerateOrder(w http.ResponseWriter, r *http.Request) {
+
+	utils.OkStatus(w, 200, "支付成功", "{\"key\":\"\"}")
 }
 
 func PaymentInfo(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +54,5 @@ func PaymentInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Header.Get("language"))
 	fmt.Println("value:", r.Form.Get("key"))
 
-	utils.OkStatus(w, 200,"支付成功", "{\"key\":\"\"}")
+	utils.OkStatus(w, 200, "支付成功", "{\"key\":\"\"}")
 }
